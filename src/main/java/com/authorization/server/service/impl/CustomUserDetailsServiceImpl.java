ccebeeea58dc272c20051079601d11aa5fc2d1e6
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-23 19:43:03 +0700

    add implementation service CustomUserDetailsServiceImpl

---
+++ src/main/java/com/authorization/server/service/impl/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,52 @@
+package com.authorization.server.service.impl;
+
+import com.authorization.server.entity.User;
+import com.authorization.server.repository.UserRepository;
+import com.authorization.server.service.CustomUserDetailsService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+@Service
+@Transactional
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                getAuthorities(Collections.singletonList(user.getRole()))
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(List<String> roles) {
+
+        // ambil semua role dari roles user, lalu masukkan kedalam object SimpleGrantedAuthority
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
+    }
+}
