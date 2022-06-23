f266552c7a32e789599d9771ec596a86db204a16
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-23 19:34:27 +0700

    add contract service CustomAuthenticationProviderService

---
+++ src/main/java/com/authorization/server/service/CustomAuthenticationProviderService.java
@@ -0,0 +1,6 @@
+package com.authorization.server.service;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+
+public interface CustomAuthenticationProviderService extends AuthenticationProvider {
+}
