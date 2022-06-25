a227980e425ee34f5c21ef544be11ae7c44572f1
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-25 08:44:30 +0700

    update entity User

--- src/main/java/com/authorization/server/entity/User.java
+++ src/main/java/com/authorization/server/entity/User.java
@@ -12,17 +12,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "email")
     private String email;
 
-    @Column(length = 60)
+    @Column(name = "password", length = 60)
     private String password;
 
+    @Column(name = "role")
     private String role;
 
+    @Column(name = "enabled")
     private boolean enabled = false;
 }
 
